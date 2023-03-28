de65de03a1c237508d1421622c95a3fef00eeaae
Oscar Rodriguez <113011957+oscarjrod@users.noreply.github.com>
2023-03-28 10:55:57 +0200

    Story 00 - Project Setup

---
+++ src/main/java/com/oscarjrod/eurderapi/EurderApiApplication.java
@@ -0,0 +1,13 @@
+package com.oscarjrod.eurderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurderApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oscarjrod/eurderapi/EurderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oscarjrod.eurderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurderApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
